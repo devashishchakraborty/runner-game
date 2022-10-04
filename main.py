e79d6b7fe4db15a8d7262eb77180b81399b12c9f
Devashish Chakraborty <devashishchakra@gmail.com>
2022-10-04 14:44:18 +0530

    jumping and gravity

--- main.py
+++ main.py
@@ -11,19 +11,20 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 # Background Surfaces
-sky_surface = pygame.image.load("graphics/Sky.png").convert()
-ground_surface = pygame.image.load("graphics/ground.png").convert()
+sky_surf = pygame.image.load("graphics/Sky.png").convert()
+ground_surf = pygame.image.load("graphics/ground.png").convert()
 
 score_surf = test_font.render("My game", False, (64, 64, 64))
 score_rect = score_surf.get_rect(center=(400, 50))
 
 # Snail Surface and Rectangle
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surface.get_rect(midbottom=(600, 300))
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom=(600, 300))
 
 # Player Surface and Rectangle
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(80, 300))
+player_gravity = 0
 
 # Main Game Loop
 while True:
@@ -34,16 +35,15 @@ while True:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                print("Jump")
+                player_gravity = -20
 
-        # Checking Collisions with event loops
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rect.collidepoint(event.pos):
-        #         print("Colliding")
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos):
+                player_gravity = -20
 
     # Displaying Background Images to the Display Surface
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
+    screen.blit(sky_surf, (0, 0))
+    screen.blit(ground_surf, (0, 300))
 
     pygame.draw.rect(screen, "#c0e8ec", score_rect)
     pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
@@ -52,23 +52,12 @@ while True:
     snail_rect.x -= 4
     if snail_rect.right < 0:
         snail_rect.left = 800
+    screen.blit(snail_surf, snail_rect)
 
-    # Added Player Models
-    screen.blit(snail_surface, snail_rect)
+    # Player
+    player_gravity += 1
+    player_rect.y += player_gravity
     screen.blit(player_surf, player_rect)
-
-    # keyboard input
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
-    #     print("jump")
-
-    # Adding Collisions
-    # if player_rect.colliderect(snail_rect):
-    #     print("collision")
-
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
