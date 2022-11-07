38427a6fbb062b45b3fb21ee33943f97479dc70d
Devashish Chakraborty <devashishchakra@gmail.com>
2022-11-07 22:03:56 +0530

    Better Enemy Spawns

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from random import randint
 
 
 def display_score():
@@ -9,6 +10,15 @@ def display_score():
     screen.blit(score_surf, score_rect)
     return current_time
 
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+
+            screen.blit(snail_surf, obstacle_rect)
+        return obstacle_list
+    else:
+        return []
 
 # Initialize Pygame
 pygame.init()
@@ -18,12 +28,6 @@ clock = pygame.time.Clock()
 
 # Font for Title
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
-
-game_name = test_font.render("Pixel Runner", False, (111, 196, 169))
-game_name_rect = game_name.get_rect(center=(400, 80))
-
-instructions_surf = test_font.render("Press SPACE to start", False, (111, 196, 169))
-instructions_rect = instructions_surf.get_rect(center=(400, 340))
 
 start_time = 0
 game_active = False
@@ -36,9 +40,11 @@ ground_surf = pygame.image.load("graphics/ground.png").convert()
 score_surf = test_font.render("My game", False, (64, 64, 64))
 score_rect = score_surf.get_rect(center=(400, 50))
 
-# Snail Surface and Rectangle
+# Obstacles
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom=(600, 300))
+
+obstacle_rect_list = []
 
 # Player Surface and Rectangle
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
@@ -49,6 +55,16 @@ player_gravity = 0
 player_stand = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
 player_stand_rect = player_stand.get_rect(center=(400, 200))
+
+game_name = test_font.render("Pixel Runner", False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center=(400, 80))
+
+instructions_surf = test_font.render("Press SPACE to start", False, (111, 196, 169))
+instructions_rect = instructions_surf.get_rect(center=(400, 340))
+
+# Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer,900)
 
 # Main Game Loop
 while True:
@@ -70,6 +86,9 @@ while True:
                 game_active = True
                 snail_rect.left = 800
                 start_time = pygame.time.get_ticks()
+        
+        if event.type == obstacle_timer and game_active:
+            obstacle_rect_list.append(snail_surf.get_rect(midbottom=(randint(900,1100), 300)))
 
     if game_active:
         # Displaying Background Images to the Display Surface
@@ -81,10 +100,10 @@ while True:
         # pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
         # screen.blit(score_surf, score_rect)
 
-        snail_rect.x -= 5
-        if snail_rect.right < 0:
-            snail_rect.left = 800
-        screen.blit(snail_surf, snail_rect)
+        # snail_rect.x -= 5
+        # if snail_rect.right < 0:
+        #     snail_rect.left = 800
+        # screen.blit(snail_surf, snail_rect)
 
         # Player
         player_gravity += 1
@@ -92,6 +111,9 @@ while True:
         if player_rect.bottom >= 300:
             player_rect.bottom = 300
         screen.blit(player_surf, player_rect)
+
+        # Obstacle Movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
 
         # Collision
         if snail_rect.colliderect(player_rect):
